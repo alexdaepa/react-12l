b0147099cced88f6b198867946d48b4f41fd542a
a44802isel <44802@alunos.isel.ipl.pt>
2021-02-19 11:43:51 +0000

    Setting up router and new components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Route exact path="/">
+        <Home></Home>
+      </Route>
+
+      <Route exact path="/stuff">
+        <h3>This is Stuff!</h3>
+        <a href="/">Go to home</a>
+      </Route>
+
+{/*
+      <Route>
+        <Redirect to="/"/>
+      </Route>
+*/}
+    </BrowserRouter>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h3>This is Home!</h3>
+            <a href="/stuff">Go to stuff</a>
+        </div>
+    );
+}
+
+export default Home;
